2a898031c2572651e9942911ca5e17316b0604a7
Shahrier Jaman <157677455+shahrierjaman@users.noreply.github.com>
2025-03-11 01:40:01 +0600

    Update products.php

--- public/products.php
+++ public/products.php
@@ -1,5 +1,12 @@
 <?php
-require '../src/db.php'; // Include database connection
+$host = getenv("DATABASE_HOST") ?: "mainline.proxy.rlwy.net";
+$user = getenv("DATABASE_USER") ?: "root";
+$pass = getenv("DATABASE_PASSWORD") ?: "edSrffrYiYSGtyMyBpOXXYVcPZriYrPo";
+$dbname = getenv("DATABASE_NAME") ?: "railway";
+$port = getenv("DATABASE_PORT") ?: "49816";
+
+// Create connection
+$conn = new mysqli($host, $user, $pass, $dbname, (int)$port); // Include database connection
 
 $sql = "SELECT * FROM products";
 $result = $conn->query($sql);
