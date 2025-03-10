5343e42e6f7a0af19db817358ed4c410fa7f87d5
Shahrier Jaman <157677455+shahrierjaman@users.noreply.github.com>
2025-03-11 00:15:43 +0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
-$host = getenv("DATABASE_HOST");
-$user = getenv("DATABASE_USER");
-$pass = getenv("DATABASE_PASSWORD");
-$dbname = getenv("DATABASE_NAME");
-$port = getenv("DATABASE_PORT");
+$host = getenv("DATABASE_HOST") ?: "mainline.proxy.rlwy.net";
+$user = getenv("DATABASE_USER") ?: "root";
+$pass = getenv("DATABASE_PASSWORD") ?: "edSrffrYiYSGtyMyBpOXXYVcPZriYrPo";
+$dbname = getenv("DATABASE_NAME") ?: "railway";
+$port = getenv("DATABASE_PORT") ?: "49816";
 
 // Create connection
-$conn = new mysqli($host, $user, $pass, $dbname, $port);
+$conn = new mysqli($host, $user, $pass, $dbname, (int)$port);
 
 // Check connection
 if ($conn->connect_error) {
