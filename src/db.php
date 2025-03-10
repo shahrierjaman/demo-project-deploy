e92388bf6365576cc72a2c355937f86579f4b008
Shahrier Jaman <157677455+shahrierjaman@users.noreply.github.com>
2025-03-11 00:16:17 +0600

    Update db.php

--- src/db.php
+++ src/db.php
@@ -1,10 +1,11 @@
 <?php
-$host = "mysql";
-$user = "root";
-$pass = "rootpassword";
-$db = "mydatabase";
+$host = getenv("DATABASE_HOST") ?: "mainline.proxy.rlwy.net";
+$user = getenv("DATABASE_USER") ?: "root";
+$pass = getenv("DATABASE_PASSWORD") ?: "edSrffrYiYSGtyMyBpOXXYVcPZriYrPo";
+$db = getenv("DATABASE_NAME") ?: "railway";
+$port = getenv("DATABASE_PORT") ?: "49816";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, (int)$port);
 
 if ($conn->connect_error) {
     die("Database Connection Failed: " . $conn->connect_error);
